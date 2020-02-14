5b676b7b22339e15181b6db32dba57d7b3378fb7
Cédric Oloa <cedric.oloa@laposte.net>
2020-02-14 13:39:43 +0100

    change redirect on success login

--- src/Security/CustomAuthAuthenticator.php
+++ src/Security/CustomAuthAuthenticator.php
@@ -94,8 +94,8 @@ class CustomAuthAuthenticator extends AbstractFormLoginAuthenticator implements 
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('easyadmin'));
+//        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
     protected function getLoginUrl()
